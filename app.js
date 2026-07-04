4cdb2e6a9c794d6d21b3d9e4a926d716c3460021
nzeenath <nzeenath2002@gmail.com>
2026-07-04 12:10:16 +0530

    3rd commit

--- app.js
+++ app.js
@@ -30,52 +30,78 @@
 
 
 
-let customer2 = {
-    name: "John",
-    age: 25,
-    address: "New York",
-    salary: 50000,
-    items: [
-        {
-            id: 1,
-            name: "item1",
-            price: 100,
-            variants: [
-                {
-                    id: 1,
-                    name: "variant1",
-                    price: 10
-                },
-                {
-                    id: 2,
-                    name: "variant2",
-                    price: 20
+// let customer2 = {
+//     name: "John",
+//     age: 25,
+//     address: "New York",
+//     salary: 50000,
+//     items: [
+//         {
+//             id: 1,
+//             name: "item1",
+//             price: 100,
+//             variants: [
+//                 {
+//                     id: 1,
+//                     name: "variant1",
+//                     price: 10
+//                 },
+//                 {
+//                     id: 2,
+//                     name: "variant2",
+//                     price: 20
                     
-                }
-            ]
-        }, {
-            id: 2,
-            name: "item2",
-            price: 200
-        }, {
-            id: 3,
-            name: "item3",
-            price: 300
-        }
-    ],
-    father: {
-        name: "Doe",
-        age: 50,
-        address: "New York"
-    }
-};
-// console.log(customer2.name);
-// console.log(customer2.age);
-// console.log(customer2.address);
-// console.log(customer2.salary);
-// console.log(customer2.items[2].name);
-// console.log(customer2.father.name);
+//                 }
+//             ]
+//         }, {
+//             id: 2,
+//             name: "item2",
+//             price: 200
+//         }, {
+//             id: 3,
+//             name: "item3",
+//             price: 300
+//         }
+//     ],
+//     father: {
+//         name: "Doe",
+//         age: 50,
+//         address: "New York"
+//     }
+// };
+// // console.log(customer2.name);
+// // console.log(customer2.age);
+// // console.log(customer2.address);
+// // console.log(customer2.salary);
+// // console.log(customer2.items[2].name);
+// // console.log(customer2.father.name);
 
-console.log(customer2.items[0].variants[1].price);
+// console.log(customer2.items[0].variants[1].price);
 
-console.log()
+// console.log()
+
+
+// document.write("<h2>Hello</h2>");
+
+console.log(document);
+
+let heading = document.getElementById("heading");
+
+heading.innerText = "ABC";
+
+let num = 0;
+function increment(){
+   
+    heading.innerText = "A"+ ++num;
+    console.log("+ Clicked");
+    
+}
+
+function decrement(){
+   
+    heading.innerText = "A"+ --num;
+    console.log(" - Clicked");
+  
+}
+
+
